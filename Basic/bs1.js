bf938e3842af4f9523eecf5878293a88e949c141
adityakgithub <adityakorade@gmail.com>
2022-10-14 16:21:01 +0530

    ForEach added

--- Basic/bs1.js
+++ Basic/bs1.js
@@ -509,6 +509,20 @@ console.log("-----------*---------")
       }
       
     // function4()
+
+    function function1() {
+        const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      
+        // traditional for loop
+        // for..of
+        // for..in
+      
+        // forEach
+        numbers.forEach((number) => {
+          console.log(number)
+        })
+    }
+    
     // Arrya Method
     // Push, Pop, splish
     // remove a value at an index
